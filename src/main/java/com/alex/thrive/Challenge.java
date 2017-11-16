39d5f896c652cf4658493d7a764e0dc25feab332
Alex Doub <alexdoub1990@gmail.com>
2017-11-16 16:15:43 -0700

    Restructured project

---
+++ src/main/java/com/alex/thrive/Challenge.java
@@ -0,0 +1,12 @@
+package com.alex.thrive;
+
+/**
+ * Created by Alex on 11/16/2017.
+ */
+
+public class Challenge {
+
+    public static void main(String[] args) {
+        System.out.println("Working");
+    }
+}
